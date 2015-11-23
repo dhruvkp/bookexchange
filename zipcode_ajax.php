2651029ea93152b8bc02abc36f4d3f41a9219a21
Dhruv Patel <dhruvkumarr.patel51@gmail.com>
2015-11-23 17:10:51 -0600

    removing zipcode redundant file

--- zipcode_ajax.php
+++ zipcode_ajax.php
@@ -1,9 +1,9 @@
 <?php
-    $db = new mysqli('localhost', 'root', 'root', 'bookexchange');
+    include 'connection/connection.php';
+    connectdb();
     $zipcode=$_GET['zipcode'];
-    $db = new mysqli('localhost', 'root', 'root', 'bookexchange');
-    $query="select city_id from Zipcode where zipcode='".$zipcode."';";
-    $result=$db->query($query);
+    $sql="select city_id from Zipcode where zipcode='".$zipcode."';";
+    $result=query($sql);
     if($result->num_rows==0)
     {
         echo 'NILL';
@@ -12,18 +12,18 @@
     {
         $row=$result->fetch_assoc();
         $city_id=$row['city_id'];
-        $query="select city_name,state_id from City where city_id='".$city_id."';";
-        $result=$db->query($query);
+        $sql="select city_name,state_id from City where city_id='".$city_id."';";
+        $result=query($sql);
         $row=$result->fetch_assoc();
         $city_name=$row['city_name'];
         $state_id=$row['state_id'];
-        $query="select state_name,country_id from State where state_id='".$state_id."';";
-        $result=$db->query($query);
+        $sql="select state_name,country_id from State where state_id='".$state_id."';";
+        $result=query($sql);
         $row=$result->fetch_assoc();
         $state_name=$row['state_name'];
         $country_id=$row['country_id'];
-        $query="select country_name from Country where country_id='".$country_id."';";
-        $result=$db->query($query);
+        $sql="select country_name from Country where country_id='".$country_id."';";
+        $result=query($sql);
         $row=$result->fetch_assoc();
         $country_name=$row['country_name'];
         echo $city_name.",".$state_name.",".$country_name;
