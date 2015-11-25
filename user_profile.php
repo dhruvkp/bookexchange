f0bd78efc3007405f3adec5d680eafe5498bd328
Dhruv Patel <dhruvkumarr.patel51@gmail.com>
2015-11-25 00:14:42 -0600

    password box text black

--- user_profile.php
+++ user_profile.php
@@ -20,7 +20,7 @@ session_start();
 <!-- <div class="row" style="font-family: 'Open Sans', sans-serif;">
            <div class="col-md-offset-2 col-md-8" style="margin-top:15px">
            <h3  align="center"> About me </h3> <hr style="margin-top:1px;margin-bottom:20px">
-        
+
            </div>
        </div> -->
 
@@ -53,7 +53,7 @@ session_start();
                                             <span>Contact</span>
                                             <text > <?php echo $row['contact_no'] ?></text>
                                         </div>
-                                        
+
                                          <div class="top">
                                             <span>Address1</span>
                                             <text > <?php echo $row['address1'] ?></text>
@@ -67,12 +67,12 @@ session_start();
                                             <?php $sql="select city_name from city where city_id =(select city_id from Zipcode where zipcode='".$row['zipcode']."');";
                                             $result1 = query($sql);
     										$row1 = $result1->fetch_assoc();
-    
+
                                              ?>
-    
+
                                             <text > <?php echo $row1['city_name'] ?></text>
                                         </div>
-                                        
+
                                            <div class="top">
                                             <span>Zipcode</span>
                                             <text > <?php echo $row['zipcode'] ?></text>
@@ -80,31 +80,31 @@ session_start();
                                         <div class="clear"> </div>
                                 </div>
                                 <div class="clear"> </div>
-                                
+
                                 <input type="submit" value="Edit Profile"> &nbsp; &nbsp;<input onclick="openPopup()" type="submit" value="Edit Password">
-                       
+
                     </div>
            </div>
       </div>
 
       <div id="pwd" style=" background-color: black;opacity:0.9; color:white">
 			   <form enctype="multipart/form-data" action="pwdchange_event.php" id="register_form" method="POST">
-                        
+
 			   <div class=" row"><div class="col-sm-offset-2" style="margin-top:25px; padding:10px">
      				 	<div class="row">
 	     				 	 <div class="col-md-3"><span>Old Password</span></div>
-			                 <div class="col-md-2"><input name="old" type="text"></div>
+			                 <div class="col-md-2"><input name="old" type="password" style="color:black;"></div>
 	                     </div><br />
                          <div class="row">
 	                     	 <div class="col-md-3"><span>New Password</span></div>
-	                         <div class="col-md-2"><input name="new" type="text"></div>
+	                         <div class="col-md-2"><input name="new" type="password" style="color:black;"></div>
 	                     </div><br />
                          <div class="row">
 	                        <div class="col-md-3"><span>Retype New Password</span></div>
-	                        <div class="col-md-2"><input name="retype" type="text"></div>
+	                        <div class="col-md-2"><input name="retype" type="password" style="color:black;"></div>
 	                     </div><br />
                           <input type="submit" class="btn btn-default" value="Submit"> </div>
-                     </div>           					   		
+                     </div>
       		  </form>
       	 </div>
 
@@ -119,44 +119,44 @@ session_start();
                     height: 330,
                     modal: true,
                     title:"change Password",
-                    
+
        });
    function openPopup()
     {
                  $("#pwd").dialog("open");
             }
-    
+
 </script>
-<!-- 
+<!--
   <div class="row">
        	<div class="col-md-offset-2 col-md-8 well">
-           
+
           <div class="col-md-offset-1 col-md-5">
-           <br><b> Name: </b> Heet Vachhani  <br> 
+           <br><b> Name: </b> Heet Vachhani  <br>
           </div>
           <div class="col-md-offset-1 col-md-5">
-           <br><b> Email: </b> 26vachhani@gmail.com <br> 
+           <br><b> Email: </b> 26vachhani@gmail.com <br>
           </div>
-         
+
          <div class="col-md-offset-1 col-md-5">
-           <br><b> City: </b>   <br> 
+           <br><b> City: </b>   <br>
           </div>
 
           <div class="col-md-offset-1 col-md-5">
-           <br><b> College: </b> <br> 
-          </div> 
-          
-          <div class="col-md-offset-1 col-md-5">
-          <br><b>Address: </b> <br> 
-          </div>   
+           <br><b> College: </b> <br>
+          </div>
 
           <div class="col-md-offset-1 col-md-5">
-           <br><b>Phone: </b> <br> 
-          </div>       
+          <br><b>Address: </b> <br>
+          </div>
+
           <div class="col-md-offset-1 col-md-5">
-           <br><b>Occupation: </b>  <br> 
-          </div> 
-          
+           <br><b>Phone: </b> <br>
+          </div>
+          <div class="col-md-offset-1 col-md-5">
+           <br><b>Occupation: </b>  <br>
+          </div>
+
           </div>
 
   </div> -->
