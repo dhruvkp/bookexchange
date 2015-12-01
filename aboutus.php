37a7fa369510ecda119976af914ea8efc03890b0
Heet Vachhani <Heet@Heets-MacBook.local>
2015-11-30 21:04:23 -0600

    last changes by heet

---
+++ aboutus.php
@@ -0,0 +1,20 @@
+<?php
+    include 'header.php';
+?>
+
+     <div class="main">
+	     <div class="row">
+			<div class="shop_top">
+	      		<div class="container" >
+    
+	      			 <div class="register-top-grid">
+                                        <h3>ABOUT US</h3><hr style="margin-top:1px;margin-bottom:20px">
+                     </div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+
+    <?php
+    include 'footer.php';
+?>
