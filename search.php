96d43d6d76387521cd059de431e013b86e111cc7
Dhruv Patel <dhruvkumarr.patel51@gmail.com>
2015-11-30 20:33:13 -0600

    filter removed from search

--- search.php
+++ search.php
@@ -5,96 +5,19 @@
 
      <div class="main">
      <div class="row">
-     <div class="col-sm-3 toppadd">
-         <div class="row  panel">
-             <div class="col-md-12 well ">
-                <p class="panelFontsize">Find books in your city:</p>
-                <br/>
-                <div class="col-md-12 ui-widget" style="font-size:12px;font-family: 'Open Sans', sans-serif;">
-                    <input id="city-filter" class="innerFont filter" type="text" placeholder="Select city"/>
 
-                </div>
-
-            </div>
-             <div class="col-md-12 well">
-                <p class="panelFontsize">Looking For:</p>
-                <br/>
-                <?php
-                    include 'connection/connection.php';
-                    connectdb();
-                    $sql="select type_name from ItemType";
-                    $res=query($sql);
-                    while($row=$res->fetch_assoc())
-                    {
-                        echo '<div class="col-md-12 "><input type="checkbox" class="filter" data-type="type_name" id="'.$row['type_name'].'" name="'.$row['type_name'].'" value="1">&nbsp;<text class="innerFont">'.$row['type_name'].'</text></div>';
-                    }
-                ?>
-
-            </div>
-             <div class="col-md-12 well">
-                <p class="panelFontsize">Posted On:</p>
-                <br/>
-                <div class="col-md-12 ">
-                    <input class="filter" type="checkbox"  id="sell" data-type="availability_type" name="Book" value="1">&nbsp;<text class="innerFont">Sell</text>
-                </div>
-                <div class="col-md-12">
-                    <input class="filter" type="checkbox" id="rent" data-type="availability_type" class="space" name="Book" value="1">&nbsp;<text class="innerFont">Rent</text>
-                </div>
-                <div class="col-md-12">
-                    <input class="filter" type="checkbox" id="exchange" data-type="availability_type" class="space" name="Book" value="1">&nbsp;<text class="innerFont">Exchange</text>
-                </div>
-            </div>
-             <div class="col-md-12 well">
-                <p class="panelFontsize">Filter by Catagory:</p>
-                <br/>
-                <?php
-                    $sql="select category_name from Category";
-                    $res=query($sql);
-                    while($row=$res->fetch_assoc())
-                    {
-                        echo '<div class="col-md-12 "><input class="filter" type="checkbox" data-type="category_name" id="'.$row['category_name'].'" class="space" name="'.$row['category_name'].'" value="1">&nbsp;<text class="innerFont">'.$row['category_name'].'</text></div>';
-                    }
-                ?>
-            </div>
-         </div>
-     </div>
-
-    <script type="text/javascript">
-
-            $( "#city-filter" ).autocomplete({
-                source: 'city-filter.php'
-            });
-
-
-        $(".filter").change(function(){
-            var myobj=new Object();
-            myobj.city=$('#city-filter').val();
-            $("input:checkbox.filter").each(function(){
-                if(this.checked){
-                    if (typeof myobj[$(this).attr('data-type')] !== 'undefined')
-                        myobj[$(this).attr('data-type')].push($(this).attr('id'));
-                    else
-                        myobj[$(this).attr('data-type')]=[$(this).attr('id')];
-                }
-            });
-            $.get('filter_results.php',myobj).done(function(data){
-                if(data=="")
-                    $('#main-container').get(0).innerHTML="<p>No items found.</p>";
-                else
-                $('#main-container').get(0).innerHTML=data;
-            });
-        });
-    </script>
-      <div class="shop_top col-md-8">
+      <div class="shop_top">
         <div class="container" id="main-container">
             <?php
+                include 'connection/connection.php';
+                connectdb();
                 $term=$_GET['search'];
                 $sql="select * from Item where post_status='available' and (title like '%".$term."%' or description like '%".$term."%' or author like '%".$term."%')";
                 $res=query($sql);
                     $cnt=0;
                     while($row=$res->fetch_assoc())
                     {
-                        if($cnt%3==0)
+                        if($cnt%4==0)
                         {
                             echo '<div class="row shop_box-top">';
                         }
@@ -132,7 +55,7 @@
 
                         echo'</div>
                     </a></div>';
-                    if($cnt%3==2)
+                    if($cnt%4==3)
                     {
                         echo '</div>';
                     }
@@ -142,6 +65,7 @@
          </div>
        </div>
     </div>
+
 </div>
 <?php
     include 'footer.php';
